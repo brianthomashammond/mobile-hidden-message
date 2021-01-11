59f1b924bc3c122d8328a50da214852fb866748c
brianthomashammond <brianthomashammond@gmail.com>
2021-01-10 17:49:40 -0800

    Turns out App.js was not uploaded. Oops.

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const startMessage = "Can you find the hidden message?";
+    const decoyMessage = "😭 This is not the hidden message. 😭";
+    const hiddenMessage = "⭐⭐⭐ You found the hidden message!!! ⭐⭐⭐";
+    const [message, setMessage] = useState(startMessage);
+    const [style, setStyle] = useState(styles.startContainer);
+    const [textStyle, setTextStyle] = useState(styles.startStyle);
+
+    const onPress = () => {
+        if (message === hiddenMessage) {
+            setMessage(startMessage);
+            setStyle(styles.startContainer);
+            setTextStyle(styles.startText);
+        } else {
+            setMessage(decoyMessage);
+        }
+    };
+
+    const onLongPress = () => {
+        setMessage(hiddenMessage);
+        setStyle(styles.winContainer);
+        setTextStyle(styles.winText);
+    };
+
+    return (
+        <View style={style}>
+            <TouchableOpacity onPress={onPress} onLongPress={onLongPress}>
+                <Text style={textStyle}>{message}</Text>
+                <StatusBar style="auto" />
+            </TouchableOpacity>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    startContainer: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    winContainer: {
+        flex: 1,
+        backgroundColor: "#000",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    startText: {
+        color: "#000",
+    },
+    winText: {
+        color: "#fff",
+    },
 });
