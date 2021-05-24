3412fb9975354044ad03889022cf7d5bec64ad0c
brianthomashammond <brianthomashammond@gmail.com>
2021-05-23 23:06:14 -0700

    formatting

--- App.js
+++ App.js
@@ -1,39 +1,41 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StatusBar } from "expo-status-bar"
+import React, { useState } from "react"
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
 
 export default function App() {
-    const startMessage = "Can you find the hidden message?";
-    const decoyMessage = "😭 This is not the hidden message. 😭";
-    const hiddenMessage = "⭐⭐⭐ You found the hidden message!!! ⭐⭐⭐";
-    const [message, setMessage] = useState(startMessage);
-    const [style, setStyle] = useState(styles.startContainer);
-    const [textStyle, setTextStyle] = useState(styles.startStyle);
+    const startMessage = "Can you find the hidden message?"
+    const decoyMessage = "😭 This is not the hidden message. 😭"
+    const hiddenMessage = "⭐⭐⭐ You found the hidden message!!! ⭐⭐⭐"
+    const [message, setMessage] = useState(startMessage)
+    const [style, setStyle] = useState(styles.startContainer)
+    const [textStyle, setTextStyle] = useState(styles.startStyle)
 
     const onPress = () => {
         if (message === hiddenMessage) {
-            setMessage(startMessage);
-            setStyle(styles.startContainer);
-            setTextStyle(styles.startText);
+            setMessage(startMessage)
+            setStyle(styles.startContainer)
+            setTextStyle(styles.startText)
         } else {
-            setMessage(decoyMessage);
+            setMessage(decoyMessage)
         }
-    };
+    }
 
     const onLongPress = () => {
-        setMessage(hiddenMessage);
-        setStyle(styles.winContainer);
-        setTextStyle(styles.winText);
-    };
+        setMessage(hiddenMessage)
+        setStyle(styles.winContainer)
+        setTextStyle(styles.winText)
+    }
 
     return (
         <View style={style}>
             <TouchableOpacity onPress={onPress} onLongPress={onLongPress}>
-                <Text style={textStyle}>{message}</Text>
+                <Text style={textStyle}>
+                    {message}
+                </Text>
                 <StatusBar style="auto" />
             </TouchableOpacity>
         </View>
-    );
+    )
 }
 
 const styles = StyleSheet.create({
@@ -55,4 +57,4 @@ const styles = StyleSheet.create({
     winText: {
         color: "#fff",
     },
-});
+})
